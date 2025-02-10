714d08673c228b53267ef54f15d3b776f1b14601
KeremsCreation <62344387+KeremsCreation@users.noreply.github.com>
2025-02-10 14:37:08 +0100

    Add files via upload

---
+++ ViewModel/CalculatorViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Calculator.Model;
+using Calculator.MVVM;
+
+namespace Calculator.ViewModel
+{
+    public class CalculatorViewModel : ViewModelBase
+    {
+        private string calcResult;
+
+        public string CalcResult
+        {
+            get { return calcResult; }
+            set
+            {
+                if (calcResult != value)
+                {
+                    calcResult = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public void ButtonClick(string buttonText)
+        {
+            CalcResult += buttonText + " ";
+        }
+
+        public void ButtonClear()
+        {
+            CalcResult = "";
+        }
+
+        public void ButtonEquals()
+        {
+            CalculatorModel c = new CalculatorModel(CalcResult);
+            CalcResult = c.Result.ToString();
+        }
+    }
+}
