76c2e26745eee3ff542a90cc8e07a84cae2ea2de
KeremsCreation <62344387+KeremsCreation@users.noreply.github.com>
2025-02-14 10:17:39 +0100

    Add files via upload

---
+++ Lib/ButtonLabels.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Lib
+{
+    public static class ButtonLabels
+    {
+        public const string Del = "Del";
+        public const string Clear = "C";
+        public const string Equals = "=";
+        //
+        public const string Sqr = "sqr()";
+        public const string Square = "^2";
+
+        public const string Mod = "%";        
+        public const string Divide = "/";
+        public const string Multiply = "*";
+        public const string Minus = "-";
+        public const string Plus = "+";
+        //
+        public const string Seven = "7";
+        public const string Eight = "8";
+        public const string Nine = "9";
+        public const string Four = "4";
+        public const string Five = "5";
+        public const string Six = "6";
+        public const string One = "1";
+        public const string Two = "2";
+        public const string Three = "3";
+        public const string Zero = "0";
+    }
+}
+
---
+++ Lib/OperatorList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Lib
+{
+
+    class OperatorList
+    {
+        public static string[] BasicOperators = [ButtonLabels.Plus, ButtonLabels.Minus, ButtonLabels.Multiply, ButtonLabels.Divide, ButtonLabels.Mod];
+        public static double Add(double a, double b) => a + b;
+        public static double Sub(double a, double b) => a - b;
+        public static double Mul(double a, double b) => a * b;
+        public static double Div(double a, double b) => b != 0 ? a / b : double.NaN;
+        public static double Mod(double a, double b) => a % b;
+        public static double Squ(double a) => a * a;
+        public static double Sqr(double a) => Math.Sqrt(a);
+    }
+}
